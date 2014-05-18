469684b394d996490a1ec0dab5e03e737d19e995
rbalestra <rbalestra@ultrabook-linux>
2014-05-18 19:11:15 -0300

    Já com os calculos

--- src/br/com/telzir/controller/PlanoController.java
+++ src/br/com/telzir/controller/PlanoController.java
@@ -1,7 +1,9 @@
 package br.com.telzir.controller;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import br.com.telzir.model.Tarifa;
@@ -14,6 +16,10 @@ import br.com.telzir.regras.CalculaTarifas;
 public class PlanoController {
 
 	private Tarifa tarifa; //Objeto Tarifa
+	private double tarifaComFaleMais;
+	private double tarifaSemFaleMais;
+	private double tarifaAplicada;
+	
 	
 	//Constutor
 	public PlanoController(){
@@ -22,24 +28,47 @@ public class PlanoController {
 
 
 
+	/** MENSAGEM */
+	public void mensagem(String mensagem){
+		 FacesMessage msg = new FacesMessage("",mensagem);
+		 FacesContext.getCurrentInstance().addMessage(null, msg);
+		
+	}
 	
 	
-	//Calcula ligação
+	//Calcula ligação - Evento do Botao Calcula
 	public void calculaLigacao(ActionEvent etv){
-		System.out.println("Plano Escolhido: " + tarifa.getPlanoEscolhido());
-		System.out.println("Origem: " + tarifa.getOrigem());
-		System.out.println("Destino: " + tarifa.getDestino());
-		System.out.println("TempoMinutos: " + tarifa.getMinutoUtilizadoLigacao());
+	
+		if (validaCampos() == false) {
+			mensagem("Preencha os os campos corretamente !");
+			return;// para o codigo
+		}
 		
 		CalculaTarifas calculaTarifas = new CalculaTarifas();
-		double tarifaFixa = calculaTarifas.buscaTarifa(tarifa.getOrigem(), tarifa.getDestino());
 		
-		System.out.println("Tarifa fixa: " + tarifaFixa);
+		int minutosUltilizados = Integer.parseInt(tarifa.getMinutoUtilizadoLigacao());
+		tarifaAplicada = calculaTarifas.pegaValorFixa(tarifa.getOrigem(), tarifa.getDestino());
+		int planoFaleMais = Integer.parseInt(tarifa.getPlanoEscolhido());
+		
+		tarifaComFaleMais = calculaTarifas.calculaPlanoComFaleMais(minutosUltilizados, tarifaAplicada, planoFaleMais);
+		tarifaSemFaleMais = calculaTarifas.calculaPlanoSemFaleMais(minutosUltilizados, tarifaAplicada);
 		
 	}
 
 
-
+	//valida campos
+	public boolean validaCampos(){
+		boolean valida = false;
+		
+		if(tarifa.getPlanoEscolhido().isEmpty() || tarifa.getOrigem().isEmpty() 
+			|| tarifa.getDestino().isEmpty() || tarifa.getMinutoUtilizadoLigacao().isEmpty()){
+			
+			valida = false;
+		} else {
+			valida = true;
+		}
+		return valida;
+	}
 
 
 	
@@ -54,9 +83,29 @@ public class PlanoController {
 		this.tarifa = tarifa;
 	}
 
+	public double getTarifaComFaleMais() {
+		return tarifaComFaleMais;
+	}
 
-	
-	
+	public void setTarifaComFaleMais(double tarifaComFaleMais) {
+		this.tarifaComFaleMais = tarifaComFaleMais;
+	}
+
+	public double getTarifaSemFaleMais() {
+		return tarifaSemFaleMais;
+	}
+
+	public void setTarifaSemFaleMais(double tarifaSemFaleMais) {
+		this.tarifaSemFaleMais = tarifaSemFaleMais;
+	}
+
+	public double getTarifaAplicada() {
+		return tarifaAplicada;
+	}
+
+	public void setTarifaAplicada(double tarifaAplicada) {
+		this.tarifaAplicada = tarifaAplicada;
+	}
 	
 
 	
--- src/br/com/telzir/regras/CalculaTarifas.java
+++ src/br/com/telzir/regras/CalculaTarifas.java
@@ -1,7 +1,6 @@
 package br.com.telzir.regras;
 
 import java.util.List;
-
 import br.com.telzir.dao.TarifaDAO;
 import br.com.telzir.model.Tarifa;
 
@@ -13,12 +12,10 @@ public class CalculaTarifas {
 	}
 
 	
-	//Busca valor Tarifa por minuto
-	public double buscaTarifa(String origem, String destino){
+	//Mostra valor da tarifa fixa de acordo com o DDD origem e destino
+	public double pegaValorFixa(String origem, String destino){
 		double valorTarifaMinuto = 0;
-		
 		TarifaDAO dao = new TarifaDAO();
-		
 		try {
 			//busca lista de tarifas no banco
 			List<Tarifa> listaTarifa = dao.listaTarifas();
@@ -35,19 +32,39 @@ public class CalculaTarifas {
 		}
 		
 		return valorTarifaMinuto; 
+	}
+	
+	
+	
+	//Calcula Plano COM FaleMais
+	public double calculaPlanoComFaleMais(int minutosLigacao, double valorTarifaFixa, int planoFaleMais){
 
-	}
-	
-	
-	/*
-	//Teste
-	public static void main(String[] args) {
-		Planos p = new Planos();
-		double valor = p.buscaTarifa("017", "011");
-		System.out.println(valor);
+		double tarifaCobrada = 0;
 		
+		if(minutosLigacao >= planoFaleMais){
+			
+			int minutosComDesconto = minutosLigacao - planoFaleMais;
+			double valorCalcula = minutosComDesconto * valorTarifaFixa;
+			tarifaCobrada = adicionalPercentual(valorCalcula);//adiona 10% no valora
+		} else {
+			tarifaCobrada = 0.0;
+		}
+		return tarifaCobrada;
 	}
-	*/
+
+	
+	//Calcula Plano SEM FaleMais
+	public double calculaPlanoSemFaleMais(int minutosLigacao, double valorTarifaFixa){
+		double valor = minutosLigacao * valorTarifaFixa;
+		return valor;
+	}
+	
+	
+	//Adicional percentual (Adiciona valor de 10%)
+	public double adicionalPercentual(double valor){
+		double percentual = (valor * 10) / 100;
+		return valor + percentual;
+	}
 	
 	
 	
